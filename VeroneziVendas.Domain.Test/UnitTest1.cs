d625d8940b29938ca0dceefd4366f76952e94de6
XGHDev <paulo.romao@unimedprudente.com.br>
2021-09-11 20:19:44 -0300

    Adicionando arquivos iniciais

---
+++ VeroneziVendas.Domain.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VeroneziVendas.Domain.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
